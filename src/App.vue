b4fc99061d371850060080e6e55057bada77b438
cookie-cong <czy835388565@163.com>
2018-11-28 20:09:30 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+ <!-- 父组件 -->
+    <input type="text" v-model="name">
+    <one :name="name"></one>
+  </div>
+</template>
+
+<script>
+import one from "./components/one"
+export default {
+  name: 'App',
+  components:{
+    one
+  },
+  data(){
+    return{
+      name:""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/one.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- 子组件 -->
+        <span>{{name}}</span>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        name:String,
+        required:true
+    }
+}
+</script>
+
